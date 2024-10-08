905c9868ebb47cde94568f001775859e109256fa
Rachana <rachanaravishekar22@gmail.com>
2024-10-08 15:06:24 +0530

    Done

---
+++ src/components/About.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import '../App.css';
+import { services } from '../constants';
+import ButtonLink from './ButtonLink';
+import Footer from './Footer';
+
+const ServiceCard = ({ service }) => (
+  <div className='sm:w-[250px] w-full'>
+    <div
+      className='w-full green-pink-gradient p-[1px] rounded-[20px]'>
+      <div
+        className='rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        style={{ background: '#151030' }}>
+        <img
+          src={service.icon}
+          alt='some_icon'
+          className='w-16 h-16 object-contain'
+        />
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {service.title}
+        </h3>
+      </div>
+    </div>
+  </div>
+);
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='bg-black h-full w-full text-white sm:flex sm:justify-around about py-12 mt-8 overflow-x-hidden' id='about'>
+        <div className='flex flex-col justify-around'>
+          <div className='sm:px-16 px-2'>
+            <h2 className='text-4xl sm:text-5xl font-extrabold mt-2'>Introduction</h2>
+            <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>👨‍💻 Hi, I'm Rachana Ravi Shekar, a soon to be 2025 graduate🎓 with a passion for deriving meaningful insights from data and solving complex business problems.
+              <br /> 
+              <br />
+              It is fascinating how mere numbers can tell stories, revealing hidden patterns and uncovering valuable insights that drive decision-making. As an aspiring data analyst, I thrive on the challenge of interpreting these stories, transforming raw data into compelling visuals, and helping businesses make informed, data-driven choices. It's not just about crunching numbers—it's about unlocking potential and shaping the future through the power of data.            </p>
+            <ButtonLink
+              url='https://drive.google.com/file/d/18tcPpmajA_Bs81TdO8UoMcqixRQPC1y9/view?usp=drive_link'
+              text='View Resume →'
+              padding={`p-3`}
+            />
+
+          </div>
+          <div className='mt-20 flex justify-center flex-wrap gap-7'>
+            {services.map((service) => (
+              <ServiceCard service={service} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,50 @@
+export const services = [
+    {
+        title: "Aspiring Data Analyst",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer Trainee",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+   /* {
+        title: "I Vlog Sometimes..",
+        icon: 'https://csgeeek.github.io/web.png',
+    }, */
+];
+
+export const name = 'Rachana';
+
+export const experiences = [
+    {
+        'company': 'Psamathe Technologies',
+        'role': 'Software Engineer Trainee',
+        'duration': 'Jun 2024 - Aug 2024',
+        'logo': 'https://media.licdn.com/dms/image/v2/D560BAQGvrOtvdXSjrA/company-logo_200_200/company-logo_200_200/0/1708616109665?e=1735776000&v=beta&t=_u5pD0BFmfkCc8s7ybWW6Z1qx3YBNVyX7LY5aAtfbg4',
+        'points': [
+            'Knowledge of OOPs concepts, enabling efficient and organized code development. ',
+            'Utilized and optimized fundamental data structures (arrays, linked lists, stacks) to solve complex problems.',
+            'Full-Stack Development: Built responsive projects using ReactJS, Express.js, and NodeJS.',
+            'MERN Stack Expertise: Successfully implemented the MERN (MongoDB, Express, React, Node) stack in various projects.'
+        ],
+        'url': 'https://inncircles.com/#home',
+    },
+   /* {
+        'company': 'AT & T',
+        'role': 'Technical Intern',
+        'duration': 'Jan 2023 - March 2023',
+        'logo': 'https://www.att.com/ecms/dam/att/consumer/global/logos/att_globe_500x500.jpg',
+        'points': [
+            'Gained experience with Git version control contributing to the development and maintenance of code repositories.',
+            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.'
+        ],
+        'url': 'https://www.att.com/',
+    }, */
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
+
